5a33ce0c3cbbfd35645623037c04608ad8355bd4
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2022-05-05 03:15:57 +0530

    srgan added

--- srgan_ass_1.py
+++ srgan_ass_1.py
@@ -73,6 +73,7 @@ HR_test, LR_test = get_data('Kolkata_020/test')
 print(HR_test.shape, LR_test.shape)
 
 
+print("Length = ************************************************** ",len(save_ind))
 for item_arr in range(len(save_ind)):
     f, ax= plt.subplots(1,2, figsize=(14, 6))
     ax[0].imshow(LR_test[item_arr], aspect='auto')
