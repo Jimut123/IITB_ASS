ed0499f58a122f496d0534969a30d8b6c1f9f3f4
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2022-05-05 10:03:59 +0530

    new dice loss adedd

--- TaskB/DiceLoss/run_kfold.py
+++ TaskB/DiceLoss/run_kfold.py
@@ -36,7 +36,95 @@ from tensorflow.keras import backend as K
 from sklearn.model_selection import KFold
 from sklearn.metrics import average_precision_score, recall_score
 
+######################### Utils
+# All the losses are present here
 
+from keras.losses import binary_crossentropy
+import keras.backend as K
+import tensorflow as tf 
+
+epsilon = 1e-5
+smooth = 1
+
+def dsc(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return score
+
+def dice_loss(y_true, y_pred):
+    loss = 1 - dsc(y_true, y_pred)
+    return loss
+
+def bce_dice_loss(y_true, y_pred):
+    loss = binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+    return loss
+
+def confusion(y_true, y_pred):
+    smooth=1
+    y_pred_pos = K.clip(y_pred, 0, 1)
+    y_pred_neg = 1 - y_pred_pos
+    y_pos = K.clip(y_true, 0, 1)
+    y_neg = 1 - y_pos
+    tp = K.sum(y_pos * y_pred_pos)
+    fp = K.sum(y_neg * y_pred_pos)
+    fn = K.sum(y_pos * y_pred_neg) 
+    prec = (tp + smooth)/(tp+fp+smooth)
+    recall = (tp+smooth)/(tp+fn+smooth)
+    return prec, recall
+
+def tp(y_true, y_pred):
+    smooth = 1
+    y_pred_pos = K.round(K.clip(y_pred, 0, 1))
+    y_pos = K.round(K.clip(y_true, 0, 1))
+    tp = (K.sum(y_pos * y_pred_pos) + smooth)/ (K.sum(y_pos) + smooth) 
+    return tp 
+
+def tn(y_true, y_pred):
+    smooth = 1
+    y_pred_pos = K.round(K.clip(y_pred, 0, 1))
+    y_pred_neg = 1 - y_pred_pos
+    y_pos = K.round(K.clip(y_true, 0, 1))
+    y_neg = 1 - y_pos 
+    tn = (K.sum(y_neg * y_pred_neg) + smooth) / (K.sum(y_neg) + smooth )
+    return tn 
+
+def tversky(y_true, y_pred):
+    y_true_pos = K.flatten(y_true)
+    y_pred_pos = K.flatten(y_pred)
+    true_pos = K.sum(y_true_pos * y_pred_pos)
+    false_neg = K.sum(y_true_pos * (1-y_pred_pos))
+    false_pos = K.sum((1-y_true_pos)*y_pred_pos)
+    alpha = 0.7
+    return (true_pos + smooth)/(true_pos + alpha*false_neg + (1-alpha)*false_pos + smooth)
+
+def tversky_loss(y_true, y_pred):
+    return 1 - tversky(y_true,y_pred)
+
+def focal_tversky(y_true,y_pred):
+    pt_1 = tversky(y_true, y_pred)
+    gamma = 0.75
+    return K.pow((1-pt_1), gamma)
+
+def dice_coef(y_true, y_pred):
+    smooth = 0.0
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+def jacard(y_true, y_pred):
+
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum ( y_true_f * y_pred_f)
+    union = K.sum ( y_true_f + y_pred_f - y_true_f * y_pred_f)
+    return intersection/union
+
+
+###########################
 
 ###################### Models
 
@@ -323,7 +411,7 @@ for train_index, test_index in kf.split(X):
     model = ModifiedUNet(height=192, width=256, n_channels=3)
 
     #model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[dice_coef, jacard, 'accuracy'])
-    model.compile(optimizer=Adam(learning_rate=1e-5),loss='binary_crossentropy',metrics=[dice_coef, jacard, Recall(), Precision(), 'accuracy'])
+    model.compile(optimizer=Adam(learning_rate=1e-5),loss=dice_loss,metrics=[dice_coef, jacard, Recall(), Precision(), 'accuracy'])
 
     saveModel(model)
 
